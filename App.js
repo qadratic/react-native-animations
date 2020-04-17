51193be6a05845c5a0810959e92f4f75a34331c0
ripudaman <ripudaman.kaushik@yahoo.in>
2020-04-18 00:44:58 +0530

    Modal implementation with blur background

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import ModalImplement from './components/ModalImplement'
+
+export default function App() {
+	return (
+		<View>
+			<ModalImplement />
+		</View>
+	)
+}
---
+++ components/ModalImplement.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { View, Text, Modal, Button, StyleSheet } from 'react-native'
+
+export default function ModalImplement() {
+	const [showModal, setShowModal] = useState(false);
+	return (
+		<View>
+			<Text>Hi there!</Text>
+			<Button title='show modal' onPress={() => { setShowModal(true) }} />
+			<Modal
+				animationType='slide'
+				visible={showModal}
+				transparent={true}
+				onRequestClose={() => {
+					setShowModal(false);
+				}}
+			>
+				<View style={styles.modalOverlay} >
+					<View style={styles.modelContent} >
+						<Text>This is modal</Text>
+						<Button title='hide modal' onPress={() => { setShowModal(false) }} />
+					</View>
+				</View>
+			</Modal>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	modalOverlay: {
+		flex: 1,
+		backgroundColor: 'rgba(0,0,0,0.5)'
+	},
+	modelContent:{
+		marginTop:'10%',
+		padding:'5%',
+		backgroundColor:'white',
+		flex:1,
+		borderTopLeftRadius:10,
+		borderTopRightRadius:10,
+	}
+});
