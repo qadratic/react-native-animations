749b268121b566af2c0edb5de4d377c51a4e5b3d
ripudaman <ripudaman.kaushik@yahoo.in>
2020-04-20 12:41:33 +0530

    added fadein fadeout feature

--- components/AnimatedImplementation.js
+++ components/AnimatedImplementation.js
@@ -1,8 +1,27 @@
 import React, { useRef } from 'react'
-import { View, Text, StyleSheet, Animated } from 'react-native'
+import { View, Text, StyleSheet, Animated, Button, Easing } from 'react-native'
 
 export default function AnimatedImplementation() {
 	const fadeAnim = useRef(new Animated.Value(0)).current;
+	const fadeIn = () => {
+		Animated.timing(
+			fadeAnim,
+			{
+				toValue: 1,
+				duration: 500,
+				easing: Easing.back()
+			}
+		).start();
+	}
+	const fadeOut = () => {
+		Animated.timing(
+			fadeAnim,
+			{
+				toValue: 0,
+				duration: 500,
+			}
+		).start();
+	}
 	return (
 		<View style={styles.conatiner} >
 			<Text>Animated component example:</Text>
@@ -10,6 +29,9 @@ export default function AnimatedImplementation() {
 			<Animated.View style={{ ...styles.animatedView, opacity: fadeAnim }} >
 				<Text>This text fades</Text>
 			</Animated.View>
+
+			<Button title='fade in' onPress={fadeIn} />
+			<Button title='fade out' onPress={fadeOut} />
 
 		</View>
 	)
